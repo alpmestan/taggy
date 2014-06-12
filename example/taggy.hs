52d5bfb90c3f11a322d75299df2f330e14d4096b
Alp Mestanogullari <alpmestan@gmail.com>
2014-06-12 15:21:18 +0200

    fix travis and make the example support several files at a time

--- example/taggy.hs
+++ example/taggy.hs
@@ -11,19 +11,18 @@ main = do
   args <- getArgs
   case args of
     ["--help"] -> usage
-    [opt, filename] | opt == "-d" || opt == "--dom" -> dom filename
-    [opt, filename] | opt == "-t" || opt == "--t" -> taggy filename
-    [filename] -> taggy filename
+    (opt : filenames) | opt == "-d" || opt == "--dom" -> mapM_ dom filenames
+    (opt : filenames) | opt == "-t" || opt == "--t" -> mapM_ taggy filenames
     _ -> usage
 
 usage :: IO ()
 usage = do
   putStrLn "taggy - simple and fast HTML parser"
   putStrLn ""
-  putStrLn "Usage:\t taggy [OPTION] <HTML file name>"
+  putStrLn "Usage:\t taggy <format> file1.html file2.html file3.html ..."
   putStrLn "\n"
-  putStrLn "Options are:"
-  putStrLn "\t -d/--dom\t Parse as a DOM tree. This isn't the default."
+  putStrLn "Formats are:"
+  putStrLn "\t -d/--dom\t Parse as a DOM tree."
   putStrLn "\t -t/--tags\t Parse as a list of opening/closing/text/comment/script/style tags"
 
 taggy :: FilePath -> IO ()
