c61e68db55bcad5b19543d8cf99cdfaeb47f34c2
Tobias Dammers <tdammers@gmail.com>
2019-01-10 23:39:54 +0100

    Handle omitted closing tags in domify.

--- tests/unit/Text/Taggy/DOMSpec.hs
+++ tests/unit/Text/Taggy/DOMSpec.hs
@@ -2,13 +2,11 @@
 
 module Text.Taggy.DOMSpec where
 
-import Data.Attoparsec.Text.Lazy
-import Data.Text.Lazy
 import Test.Hspec
-import Text.Taggy.Parser
 import Text.Taggy.DOM
 import Text.Taggy.Types
 import Data.Monoid (mempty)
+import Data.Text (Text)
 
 spec :: Spec
 spec = do
@@ -18,8 +16,8 @@ spec = do
       , TagText "pizza"
       ]
       `domifiesTo`
-      [ NodeElement (Element "br" mempty [])
-      , NodeContent "pizza"
+      [ e "br" []
+      , text "pizza"
       ]
     it "domifies paired tags" $
       [ TagOpen "p" [] False
@@ -28,8 +26,24 @@ spec = do
       , TagText "olives"
       ]
       `domifiesTo`
-      [ NodeElement (Element "p" mempty [NodeContent "pizza"])
-      , NodeContent "olives"
+      [ e "p" [ text "pizza" ]
+      , text "olives"
+      ]
+    it "domifies omitted closing tags at end" $
+      [ TagOpen "p" [] False
+      , TagText "pizza"
+      ]
+      `domifiesTo`
+      [ e "p" [text "pizza"]
+      ]
+    it "closes tags when parent closes" $
+      [ TagOpen "p" [] False
+      , TagOpen "em" [] False
+      , TagText "pizza"
+      , TagClose "p"
+      ]
+      `domifiesTo`
+      [ e "p" [ e "em" [ text "pizza" ] ]
       ]
     it "domifies implicitly closed <p>" $
       [ TagOpen "p" [] False
@@ -38,8 +52,8 @@ spec = do
       , TagText "olives"
       ]
       `domifiesTo`
-      [ NodeElement (Element "p" mempty [NodeContent "pizza"])
-      , NodeElement (Element "p" mempty [NodeContent "olives"])
+      [ e "p" [text "pizza"]
+      , e "p" [text "olives"]
       ]
     it "domifies implicitly closed <li>" $
       [ TagOpen "ul" [] False
@@ -50,14 +64,39 @@ spec = do
       , TagClose "ul"
       ]
       `domifiesTo`
-      [ NodeElement (Element "ul" mempty
-          [ NodeElement (Element "li" mempty
-              [ NodeContent "item 1" ]
-          , NodeElement (Element "li" mempty
-              [ NodeContent "item 2" ]
+      [ e "ul"
+          [ e "li"
+              [ text "item 1" ]
+          , e "li"
+              [ text "item 2" ]
+          ]
+      ]
+    it "domifies implicitly closed <th>, <td>, <tr>" $
+      [ TagOpen "table" [] False
+      , TagOpen "tr" [] False
+      , TagOpen "th" [] False
+      , TagOpen "td" [] False
+      , TagOpen "td" [] False
+      , TagOpen "tr" [] False
+      , TagClose "table"
+      ]
+      `domifiesTo`
+      [ e "table"
+          [ e "tr"
+              [ e "th" []
+              , e "td" []
+              , e "td" []
+              ]
+          , e "tr" []
           ]
       ]
 
 domifiesTo :: [Tag] -> [Node] -> Bool
 domifiesTo tags expected =
   domify tags == expected
+
+e :: Text -> [Node] -> Node
+e tag children = NodeElement $ Element tag mempty children
+
+text :: Text -> Node
+text txt = NodeContent txt
